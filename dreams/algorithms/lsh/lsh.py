1fdffa2361c32f6536a2a9064f2e7719f77482c3
roman-bushuiev <ro3721@gmail.com>
2024-07-14 22:08:47 +0200

    Use SHA256 for LSH conversion

--- dreams/algorithms/lsh/lsh.py
+++ dreams/algorithms/lsh/lsh.py
@@ -1,4 +1,5 @@
 import numpy as np
+import hashlib
 from tqdm import tqdm
 from dreams.utils.spectra import bin_peak_list, bin_peak_lists
 
@@ -6,24 +7,39 @@ from dreams.utils.spectra import bin_peak_list, bin_peak_lists
 class RandomProjection:
     def __init__(self, n_elems: int, n_hyperplanes: int, seed=3):
         np.random.seed(seed)
-        if (n_hyperplanes % 64) != 0: 
-            n_hyperplanes_ = round(n_hyperplanes / 64) * 64 if n_hyperplanes > 64 else 64
-            print(f"n_hyperplanes ({n_hyperplanes}) must be positive and divisible by 64. rounding it to {n_hyperplanes_}.")
-            n_hyperplanes = n_hyperplanes_
+        # if (n_hyperplanes % 64) != 0: 
+        #     n_hyperplanes_ = round(n_hyperplanes / 64) * 64 if n_hyperplanes > 64 else 64
+        #     print(f"n_hyperplanes ({n_hyperplanes}) must be positive and divisible by 64. rounding it to {n_hyperplanes_}.")
+        #     n_hyperplanes = n_hyperplanes_
 
         self.H = np.random.randn(n_hyperplanes, n_elems)
 
-    def compute(self, x: np.array, as_int=True, batched=False):
+    def compute(self, x: np.array, as_str=True, batched=False):
         if batched:
             proj_signs = np.einsum('ij,kj->ki', self.H, x) >= 0
         else:
             proj_signs = self.H @ x >= 0
 
-        if as_int:
-            # Binary representation of a boolean array as integer    
-            proj_signs_u8 = proj_signs.astype(np.uint8) # N, n_hyp
-            proj_signs_u64 = np.packbits(proj_signs_u8, axis=1).view(np.uint64) # N, (n_hyp//8)
-            proj_signs = np.bitwise_xor.reduce(proj_signs_u64, axis=1) # N,
+        if as_str:
+            # Convert each row of the boolean array to bytes and hash using SHA-256
+            proj_signs_hashed = np.apply_along_axis(
+                lambda row: hashlib.sha256(row.tobytes()).hexdigest(), 1, proj_signs
+            )
+            return proj_signs_hashed
+            
+        return proj_signs
+
+        # if as_int:
+        #     # Binary representation of a boolean array as integer 
+        #     print('-'*50)   
+        #     proj_signs_u8 = proj_signs.astype(np.uint8) # N, n_hyp
+        #     print(proj_signs_u8.shape)
+        #     proj_signs_u64 = np.packbits(proj_signs_u8, axis=1).view(np.uint64) # N, (n_hyp//8)
+        #     print(proj_signs_u64.shape)
+        #     print(proj_signs_u64[0])
+        #     proj_signs = np.bitwise_xor.reduce(proj_signs_u64, axis=1) # N,
+        #     print(proj_signs[0])
+
         return proj_signs
 
 
@@ -63,7 +79,7 @@ class BatchedPeakListRandomProjection(PeakListRandomProjection):
         lshs = []
         batch_idx = range(0, n, self.subbatch_size)
         
-        with tqdm(total=n, disable=not progress_bar) as pbar:
+        with tqdm(total=n, disable=not progress_bar, desc='Computing LSHs') as pbar:
             for i in batch_idx:
                 if logger:
                     logger.info(f'Computing LSH for batch [{i}:{i+self.subbatch_size}] (out of {n})...')
