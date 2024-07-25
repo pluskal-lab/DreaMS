d1808fbae1395179dc21c16a71bf0df9d133b7d0
roman-bushuiev <ro3721@gmail.com>
2024-07-25 19:44:00 +0200

    Add legacy io.lcmsms_to_hdf5

--- dreams/utils/io.py
+++ dreams/utils/io.py
@@ -16,6 +16,9 @@ import contextlib
 import time
 import io as std_io
 import wandb
+import pyopenms as pyms
+import traceback
+from collections import Counter
 from functools import cache
 from matchms.importing import load_from_mgf
 from pathlib import Path
@@ -25,6 +28,7 @@ from itertools import groupby
 from tqdm import tqdm
 import dreams.utils.spectra as su
 import dreams.utils.misc as utils
+import dreams.utils.lcms as lcms
 from dreams.definitions import *
 
 
@@ -461,6 +465,390 @@ def read_mzml(pth: Union[Path, str], verbose=False):
 
     df = pd.DataFrame(df)
     return df
+
+
+def lcmsms_to_hdf5(
+        input_path,
+        output_path=None,
+        num_peaks=None,
+        num_prec_peaks=None,
+        store_precursors=True,
+        compress_peaks_lvl=0,
+        compress_full_lvl=0,
+        pwiz_stats=False,
+        del_in=False,
+        verbose=False,
+        log_path=None
+    ):
+
+    # If log_path is None then logger prints to stdout
+    logger = setup_logger(log_path)
+
+    # Parse the input file
+    df_msn_data, df_prec_data, file_props = read_lcmsms(
+        input_path=input_path,
+        store_precursors=store_precursors,
+        pwiz_stats=pwiz_stats,
+        verbose=verbose,
+        logger=logger
+    )
+
+    # Write the parsed data to the output .hdf5 file
+    if df_msn_data is not None:
+        if not output_path:
+            output_path = os.path.splitext(input_path)[0] + '.hdf5'
+
+        parsed_lcmsms_to_hdf(
+            output_path=output_path,
+            file_props=file_props,
+            df_msn_data=df_msn_data,
+            df_prec_data=df_prec_data,
+            logger=logger,
+            num_peaks=num_peaks,
+            num_prec_peaks=num_prec_peaks,
+            compress_peaks_lvl=compress_peaks_lvl,
+            compress_full_lvl=compress_full_lvl
+        )
+
+    # Delete input file
+    if del_in:
+        os.remove(input_path)
+
+
+def read_lcmsms(
+        input_path,
+        logger,
+        store_precursors=True,
+        pwiz_stats=False,
+        verbose=False
+    ):
+
+    logger.info(f'Started processing {input_path}')
+
+    # File properties that are to be stored in .hdf5
+    file_props = {'name': os.path.basename(input_path)}
+
+    # Load ms data file
+    msdata = pyms.MSExperiment()
+    try:
+        pyms.FileHandler().loadExperiment(input_path, msdata)
+    except Exception:
+        logger.error('\nPARSING ERROR\n' + traceback.format_exc() + '\nPARSING ERROR')
+        logger.info('INPUT FILE')
+        with open(input_path, 'r') as f:
+            for line in f.readlines():
+                logger.info(line)
+        logger.info('INPUT FILE')
+        #write_to_hdf(args, file_props=None, df_msn_data=None, prec_spectra_data=None, logger=logger)
+        return None, None, None
+
+    file_props['Ordered RT'] = lcms.sorted_by_rt(msdata)
+    if not file_props['Ordered RT']:
+        lcms.sort_by_rt(msdata)
+
+    # Get order of spectra property
+    order_of_spectra = lcms.get_order_of_spectra(msdata)
+    file_props['MSLevelOrder'] = order_of_spectra.name
+
+    # If order of spectra is invalid or insufficient do not continue the processing
+    if order_of_spectra in [lcms.MSLevelsOrder.INVALID, lcms.MSLevelsOrder.EMPTY,
+                            lcms.MSLevelsOrder.SINGLE_MS1, lcms.MSLevelsOrder.UNIFORM_MS1]:
+        logger.info(f'Not processing the file because of {order_of_spectra}')
+        #write_to_hdf(args, file_props=file_props, df_msn_data=None, prec_spectra_data=None, logger=logger)
+        return None, None, None
+
+    # Update file_props dict with instrument properties
+    file_props.update(lcms.get_instrument_props(msdata))
+
+    # Dict mapping ms_level to the last spectrum of this level
+    # e.g. {1: pyopenms.Spectrum, 2: pyopenms.Spectrum}
+    prev_spectra = {}
+    prev_spectrum = None
+    prec_spectra_data = {'peak list': [], 'RT': [], 'scan id': [], 'ion injection time': []}
+
+    ms1_n, msn_n = 0, 0
+    spectra_data = []
+    problems, prec_problems = Counter(), Counter()
+    for scan_id, spectrum in enumerate(msdata):
+
+        spectrum_data = {}
+
+        ms_level = spectrum.getMSLevel()
+        rt = spectrum.getRT()
+
+        # Ion injection time
+        acq_info = spectrum.getAcquisitionInfo()
+        inject_t = acq_info[0].getMetaValue('MS:1000927') if acq_info is not None and acq_info.size() > 0 else -1
+
+        if ms_level == 1:
+            ms1_n += 1
+
+        # Get precursor spectrum (last spectrum with lower MS level)
+        if prev_spectrum is not None:
+            prev_spectra[prev_spectrum.getMSLevel()] = {
+                # id - position in prec_spectra_data
+                'peak list': prev_spectrum, 'RT': rt, 'scan id': scan_id, 'id': None, 'ion injection time': inject_t
+            }
+        prev_spectrum = spectrum
+
+        # Consider only MSn spectra, where n > 1
+        if ms_level > 1:
+
+            msn_n += 1
+
+            # I. Process peak list
+
+            peaks = spectrum.get_peaks()
+            if not peaks:
+                continue
+            mzs = peaks[0]
+            intensities = peaks[1]
+
+            peak_list = su.process_peak_list(np.array([mzs, intensities]), sort_mzs=True)
+
+            # II. Check that the peak list is valid
+
+            spec_type = lcms.get_spectrum_type(spectrum)
+            problems_list = su.is_valid_peak_list(peak_list, return_problems_list=True, relative_intensities=False)
+            if problems_list:
+                if verbose:
+                    logger.error('Errors in MSn scan {}: {} ({})'.format(
+                        scan_id,
+                        json.dumps(problems_list),
+                        spec_type
+                    ))
+                for p in problems_list:
+                    problems[p] += 1
+                continue
+            else:
+                spectrum_data['peak list'] = peak_list
+
+            # III. Collect peak list metadata
+
+            # > MS level
+            spectrum_data['MS level'] = ms_level
+
+            # > Retention time
+            spectrum_data['RT'] = rt
+
+            # > Ion injection time
+            spectrum_data['ion injection time'] = inject_t
+
+            # > Polarity
+            polarity = spectrum.getInstrumentSettings().getPolarity()
+            if polarity == pyms.IonSource.Polarity.POSITIVE:
+                spectrum_data['positive polarity'] = 1
+            elif polarity == pyms.IonSource.Polarity.NEGATIVE:
+                spectrum_data['positive polarity'] = 0
+            else:
+                spectrum_data['positive polarity'] = -1
+
+            # > Spectrum type (profile/centroid)
+            spectrum_data['type'] = spec_type.value
+            # spectrum_data['type estim'] = lcms.estimate_peak_list_type(peak_list, to_int=True)
+
+            # > Scan definition string (for Thermo instruments)
+            scan_def = spectrum.getMetaValue('filter string')
+            spectrum_data['def str'] = scan_def if scan_def is not None else ''
+
+            # Analyze precursor properties and precursor spectrum
+            precursors = spectrum.getPrecursors()
+            # NOTE: is there at least one case where len(precursors) > 1?
+            if len(precursors) > 1:
+                logger.info(f'len(precursors) > 1 at scan {scan_id}')
+            if precursors and precursors[0] is not None:
+
+                # > Charge
+                spectrum_data['charge'] = precursors[0].getCharge()
+
+                # > Precursor m/z
+                spectrum_data['precursor mz'] = precursors[0].getMZ()
+
+                # > Isolation window offsets
+                spectrum_data['window lo'] = precursors[0].getIsolationWindowLowerOffset()
+                spectrum_data['window uo'] = precursors[0].getIsolationWindowUpperOffset()
+
+                # > Activation energy
+                spectrum_data['energy'] = precursors[0].getActivationEnergy()
+
+                # > Precursor spectrum (peak list, scan id, RT)
+                if store_precursors and ms_level - 1 in prev_spectra:
+
+                    prec_spectrum = prev_spectra[ms_level - 1]['peak list']
+
+                    if prev_spectra[ms_level - 1]['id'] is None:
+                        prec_peaks = prec_spectrum.get_peaks()
+                        prec_pl = su.process_peak_list(np.array([prec_peaks[0], prec_peaks[1]]), sort_mzs=True)
+                        prec_problems_list = su.is_valid_peak_list(prec_pl, return_problems_list=True,
+                                                                   relative_intensities=False)
+                        if prec_problems_list:
+                            if verbose:
+                                logger.error('Errors in precursor scan {}: {} ({})'.format(
+                                    prev_spectra[ms_level - 1]["scan id"],
+                                    json.dumps(prec_problems_list),
+                                    spec_type
+                                ))
+                            for p in prec_problems_list:
+                                prec_problems[p] += 1
+                            continue
+                        else:
+                            prev_spectra[ms_level - 1]['id'] = len(prec_spectra_data['peak list'])
+                            prec_spectra_data['peak list'].append(prec_pl)
+                            prec_spectra_data['scan id'].append(prev_spectra[ms_level - 1]['scan id'])
+                            prec_spectra_data['RT'].append(prev_spectra[ms_level - 1]['RT'])
+                            prec_spectra_data['ion injection time'].append(
+                                prev_spectra[ms_level - 1]['ion injection time']
+                            )
+
+                    spectrum_data['precursor id'] = prev_spectra[ms_level - 1]['id']
+
+            spectra_data.append(spectrum_data)
+
+    if pwiz_stats:
+        logger.info(f'ProteoWizard conversion statistics: {json.dumps(lcms.get_pwiz_stats(msdata))}')
+
+    logger.info(f'Num. of MS1 spectra: {ms1_n}')
+    logger.info('Collected {} from {} total num. of MSn spectra'.format(
+        len(spectra_data),
+        msn_n
+    ))
+
+    # Create dataframe out of MSn and MS1 data
+    df_msn_data = pd.DataFrame(spectra_data)
+    df_prec_data = pd.DataFrame(prec_spectra_data)
+
+    if verbose:
+        logger.info(f'File properties: {json.dumps(file_props)}')
+
+    logger.info(f'Spectra problems: {json.dumps(problems)}')
+    logger.info(f'Precursor spectra problems: {json.dumps(prec_problems)}')
+
+    if len(spectra_data) > 0:
+        if verbose:
+            logger.info(f'Positive polarity: {df_msn_data["positive polarity"].value_counts().to_json()}')
+            logger.info(f'Charge: {df_msn_data["charge"].value_counts().to_json()}')
+            logger.info(f'Type hist: {df_msn_data["type"].value_counts().to_json()}')
+            # logger.info(f'Type estim hist: {df_msn_data["type estim"].value_counts().to_json()}')
+        return df_msn_data, df_prec_data, file_props
+    else:
+        logger.warning('No MSn spectra collected, not storing .hdf5 file.')
+
+    logger.info(f'Finished processing {input_path}')
+
+
+def parsed_lcmsms_to_hdf(
+        output_path,
+        file_props,
+        df_msn_data,
+        df_prec_data,
+        logger,
+        num_peaks=None,
+        num_prec_peaks=None,
+        compress_peaks_lvl=3,
+        compress_full_lvl=3,
+    ):
+    # Create a new .hdf5 file to store the data
+    with h5py.File(output_path, 'w') as hdf_file:
+
+        # Fill hdf5 attributes with the file properties
+        if file_props is not None:
+            for k, v in file_props.items():
+                hdf_file.attrs[k] = v if v is not None else -1
+
+        # Create hdf5 datasets for peak lists and their metadata
+        if df_msn_data is not None:
+
+            # Fill NaN values with -1
+            pd.set_option('future.no_silent_downcasting', True)  # TODO: is there a better solution?
+            df_msn_data = df_msn_data.fillna(-1)
+
+            # Find maximum number of peaks in peak list
+            peaks_n = df_msn_data['peak list'].apply(lambda pl: pl.shape[1])
+            max_peaks_n = peaks_n.max()
+
+            # Trim peak lists to num_peaks peaks if num_peaks is set
+            if num_peaks and max_peaks_n > num_peaks:
+                logger.info('Trimming MSn peaks to {} peaks (max was {}, mean was {}, median was {}).'.format(
+                    num_peaks,
+                    max_peaks_n,
+                    peaks_n.mean(),
+                    peaks_n.median()
+                ))
+                max_peaks_n = num_peaks
+                df_msn_data['peak list'] = df_msn_data['peak list'].apply(lambda pl: su.trim_peak_list(pl, max_peaks_n))
+
+            # Pad peak lists with 0 up to the maximum
+            df_msn_data['peak list'] = df_msn_data['peak list'].apply(lambda pl: su.pad_peak_list(pl, max_peaks_n))
+            peak_lists = np.stack(df_msn_data['peak list'])
+
+            # Create datasets for MSn data
+            msn_group = hdf_file.create_group('MSn data')
+            msn_group.create_dataset('mzs', data=peak_lists[:, 0, :], dtype='f8', compression='gzip',
+                                     compression_opts=compress_peaks_lvl)
+            msn_group.create_dataset('intensities', data=peak_lists[:, 1, :], dtype='f4', compression='gzip',
+                                     compression_opts=compress_peaks_lvl)
+            msn_group.create_dataset('MS level', data=df_msn_data['MS level'], dtype='i1',
+                                     compression=compress_full_lvl)
+            msn_group.create_dataset('RT', data=df_msn_data['RT'], dtype='f4', compression=compress_full_lvl)
+            msn_group.create_dataset('charge', data=df_msn_data['charge'], dtype='i1',
+                                     compression=compress_full_lvl)
+            msn_group.create_dataset('positive polarity', data=df_msn_data['positive polarity'], dtype='i1',
+                                     compression=compress_full_lvl)
+            msn_group.create_dataset('precursor mz', data=df_msn_data['precursor mz'], dtype='f4',
+                                     compression=compress_full_lvl)
+            msn_group.create_dataset('window lo', data=df_msn_data['window lo'], dtype='f4',
+                                     compression=compress_full_lvl)
+            msn_group.create_dataset('window uo', data=df_msn_data['window uo'], dtype='f4',
+                                     compression=compress_full_lvl)
+            msn_group.create_dataset('energy', data=df_msn_data['energy'], dtype='f4',
+                                     compression=compress_full_lvl)
+            msn_group.create_dataset('ion injection time', data=df_msn_data['ion injection time'], dtype='f4',
+                                     compression=compress_full_lvl)
+            msn_group.create_dataset('type', data=df_msn_data['type'], dtype='i1', compression=compress_full_lvl)
+            # msn_group.create_dataset('type estim', data=df_msn_data['type estim'], dtype='i1',
+            #                          compression=compress_full_lvl)
+            msn_group.create_dataset('def str', data=df_msn_data['def str'], dtype=h5py.string_dtype('utf-8', None),
+                                     compression=compress_full_lvl)
+
+            # Create hdf5 datasets for the data of precursor spectra
+            if df_prec_data is not None:
+
+                # Create dataset with pointers from MSn spectra to their precursors
+                msn_group.create_dataset('precursor id', data=df_msn_data['precursor id'], dtype='i4',
+                                         compression=compress_full_lvl)
+
+                # Find maximum number of peaks in peak list and pad peak lists with 0 up to the maximum
+                prec_peak_lists = pd.Series(df_prec_data['peak list'])
+                prec_peaks_n = prec_peak_lists.apply(lambda pl: pl.shape[1])
+                max_peaks_n = prec_peaks_n.max()
+                if num_prec_peaks and max_peaks_n > num_prec_peaks:
+                    logger.info('Trimming precursor peaks to {} (max was {}, mean was {}, median was {}).'.format(
+                        num_prec_peaks,
+                        max_peaks_n,
+                        prec_peaks_n.mean(),
+                        prec_peaks_n.median()
+                    ))
+                    max_peaks_n = num_prec_peaks
+                    prec_peak_lists = prec_peak_lists.apply(lambda pl: su.trim_peak_list(pl, max_peaks_n))
+                prec_peak_lists = prec_peak_lists.apply(lambda pl: su.pad_peak_list(pl, max_peaks_n))
+                prec_peak_lists = np.stack(prec_peak_lists)
+
+                # Create datasets
+                prec_group = hdf_file.create_group('precursor data')
+                prec_group.create_dataset('mzs', data=prec_peak_lists[:, 0, :], dtype='f8', compression='gzip',
+                                          compression_opts=compress_peaks_lvl)
+                prec_group.create_dataset('intensities', data=prec_peak_lists[:, 1, :], dtype='f4', compression='gzip',
+                                          compression_opts=compress_peaks_lvl)
+                prec_group.create_dataset('RT', data=df_prec_data['RT'], dtype='f4',
+                                          compression=compress_full_lvl)
+                prec_group.create_dataset('ion injection time', data=df_prec_data['ion injection time'],
+                                          dtype='f4', compression=compress_full_lvl)
+                prec_group.create_dataset('scan id', data=df_prec_data['scan id'], dtype='i4',
+                                          compression=compress_full_lvl)
+                prec_group.create_dataset('type', data=df_msn_data['type'], dtype='i1',
+                                          compression=compress_full_lvl)
+                prec_group.create_dataset('def str', data=df_msn_data['def str'],
+                                          dtype=h5py.string_dtype('utf-8', None), compression=compress_full_lvl)
 
 
 def save_nist_like_df_to_mgf(df, out_pth: Path, remove_mol_info=False):
