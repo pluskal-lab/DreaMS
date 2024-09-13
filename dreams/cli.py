4000683302b1999b7f53a56a630408eacaa4f970
roman-bushuiev <ro3721@gmail.com>
2024-09-13 14:43:01 +0200

    Update CLI

--- dreams/cli.py
+++ dreams/cli.py
@@ -6,5 +6,6 @@ if __name__ == '__main__':
     fire.Fire({
         'lcmsms_to_hdf5': io.lcmsms_to_hdf5,
         'downloadpublicdata_to_hdf5s': io.downloadpublicdata_to_hdf5s,
-        'merge_lcmsms_hdf5s': io.merge_lcmsms_hdf5s
+        'merge_lcmsms_hdf5s': io.merge_lcmsms_hdf5s,
+        'compress_hdf5': io.compress_hdf,
     })
