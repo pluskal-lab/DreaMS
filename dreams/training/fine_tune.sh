04c7c8b6dd6b5c8ef605c8daa923daeb03fa6a0d
roman-bushuiev <ro3721@gmail.com>
2024-09-22 11:22:35 +0200

    Add `Contrastive fine-tuning` to fine_tune.sh

--- dreams/training/fine_tune.sh
+++ dreams/training/fine_tune.sh
@@ -41,3 +41,31 @@ python3 training/train.py \
  --val_check_interval 0.1 \
  --max_peaks_n 100 \
  --save_top_k -1
+
+# Contrastive fine-tuning
+# python3 training/train.py \
+#  --project_name CONTRASTIVE_FINE_TUNING \
+#  --job_key "lr5e-6_margin0.1_fixed_rel_intens_max_peaks_n100" \
+#  --run_name "lr5e-6_margin0.1_fixed_rel_intens_max_peaks_n100" \
+#  --train_objective contrastive_spec_embs \
+#  --train_regime fine-tuning \
+#  --dformat A \
+#  --model DreaMS \
+#  --lr 5e-6 \
+#  --batch_size 4 \
+#  --prec_intens 1.1 \
+#  --num_devices 8 \
+#  --max_epochs 301 \
+#  --log_every_n_steps 5 \
+#  --seed 3407 \
+#  --train_precision 32 \
+#  --val_check_interval 1.0 \
+#  --save_top_k -1 \
+#  --head_depth 0 \
+#  --unfreeze_backbone_at_epoch 0 \
+#  --dataset_pth "${MERGED_DATASETS}/MoNA_A_Murcko_split_neighbours_[M+H]+_0.05Da.pkl" \
+#  --pre_trained_pth "${PRETRAINED}/ssl_model.ckpt" \
+#  --n_pos_samples 1 \
+#  --n_neg_samples 1 \
+#  --triplet_loss_margin 0.1 \
+#  --max_peaks_n 100
