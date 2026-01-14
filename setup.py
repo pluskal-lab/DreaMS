de0425abbe05eda16f09eec382c10bf2b29ed985
roman-bushuiev <ro3721@gmail.com>
2026-01-14 23:28:54 +0100

    Upgrade `huggingface_hub`

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
         "igraph==0.11.4",
         "molplotly==1.1.7",
         "fire==0.6.0",
-        "huggingface_hub==0.24.5",
+        "huggingface_hub==0.28.1",
         "msml @ git+https://github.com/roman-bushuiev/msml_legacy_architectures.git@main"
     ],
     extras_require={
